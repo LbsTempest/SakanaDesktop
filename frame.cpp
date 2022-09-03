3d1428a0bb496649e454af43318110649b9eb61a
Tempest <l1641101764@gmail.com>
2022-09-04 00:35:30 +0800

    change

--- frame.cpp
+++ frame.cpp
@@ -5,6 +5,7 @@
 #include "frame.h"
 #include "QPainter"
 #include "spring_point.h"
+#include "QPainterPath"
 
 spring_point s_point;
 frame::frame() {
@@ -15,9 +16,14 @@ frame::frame() {
 
 void frame::paintEvent(QPaintEvent *event) {
     QPainter sakana_painter(this);
+    sakana_painter.setRenderHint(QPainter::Antialiasing);
 //    sakana_painter.fillRect(0, 200, 300, 200, Qt::color1);
     sakana_painter.setPen(QPen(Qt::red, 10));
-    sakana_painter.drawPoint(s_point.get_position().get_x(), s_point.get_position().get_y());
+//    sakana_painter.drawPoint(s_point.get_position().get_x(), s_point.get_position().get_y());
     s_point.update_();
 
+    QPainterPath path_painter;
+    path_painter.moveTo(s_point.get_position().get_x(), s_point.get_position().get_y());
+    path_painter.quadTo(200, 300, 200, 400);
+    sakana_painter.drawPath(path_painter);
 }
--- spring_point.cpp
+++ spring_point.cpp
@@ -9,7 +9,7 @@ num_vector spring_point::get_position() {
 }
 
 num_vector spring_point::compute_force() {
-    double k1 = 1, k2 = 1, mu = 0.01;
+    double k1 = 1, k2 = 1, mu = 0.1;
     num_vector position_(position.get_x() - 200, (position.get_y() - 200) / 1.25);
     num_vector force = -position_ * k1 - speed * mu + NEGATIVE_Y_VECTOR * 20;
 //    double sin = position_.get_x() / position_.length(), cos = position_.get_y() / position_.length();
